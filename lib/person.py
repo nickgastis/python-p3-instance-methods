8cd55e91d7c56d32ceabda937f07efd5ca492714
Nick Gastis <nickgastis1@gmail.com>
2023-04-04 09:41:06 -0400

    done

--- lib/person.py
+++ lib/person.py
@@ -1,7 +1,13 @@
 #!/usr/bin/env python3
 
 class Person:
-    # Class body goes here
+    def talk(self):
+        print('Hello World!')
+    def walk(self):
+        print('The person is walking.')
+    
+dave = Person()
+dave.talk()
+dave.walk()
 
-    #Instance method definition
-    pass
+
